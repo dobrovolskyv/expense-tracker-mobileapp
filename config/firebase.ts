d6e72884170bbe8b2885dd8636a22ccd86c5013c
Vlados <malak3@mail.ru>
2025-01-21 02:51:55 +0300

    bugs

--- config/firebase.ts
+++ config/firebase.ts
@@ -6,6 +6,15 @@ import { initializeAuth, /*getReactNativePersistence,*/ getAuth } from "firebase
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from "firebase/firestore";
 
+// import {
+//     REACT_APP_API_KEY,
+//     REACT_APP_AUTH_DOMAIN,
+//     REACT_APP_PROJECT_ID,
+//     REACT_APP_STORAGE_BUCKET,
+//     REACT_APP_MESSAGING_SENDER_ID,
+//     REACT_APP_APP_ID
+// } from "@env"
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,6 +25,14 @@ const firebaseConfig = {
     messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
     appId: process.env.EXPO_PUBLIC_APP_ID
 };
+// const firebaseConfig = {
+//     apiKey: REACT_APP_API_KEY,
+//     authDomain: REACT_APP_AUTH_DOMAIN,
+//     projectId: REACT_APP_PROJECT_ID,
+//     storageBucket: REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+//     appId: REACT_APP_APP_ID
+// };
 
 
 // Initialize Firebase
