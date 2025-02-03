b6528639f2d4d2d5312ddb46c5b186a083f52c02
Vlados <malak3@mail.ru>
2025-02-03 19:22:52 +0300

    add home styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,14 +2,15 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Button from '@/components/Button'
 import Typograph from '@/components/Typograph'
-import { colors } from '@/constants/theme'
+import { colors, spacingX, spacingY } from '@/constants/theme'
 import { signOut } from 'firebase/auth'
 import { auth } from '@/config/firebase'
 import ScreenWrapper from '@/components/ScreenWrapper'
 import { useAuth } from '@/contexts/authContext'
+import { verticalScale } from '@/utils/styling'
 
 const Home = () => {
-    // const {user} = useAuth()
+    const {user} = useAuth() 
 
     // const handleLogout = async () => {
     //     await signOut(auth)
@@ -18,13 +19,41 @@ const Home = () => {
     return (
         <ScreenWrapper>
             <Typograph>Home</Typograph>
-            {/* <Button onPress={handleLogout}>
-                <Typograph color={colors.black}>Выход</Typograph>
-            </Button> */}
+        
         </ScreenWrapper>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        paddingHorizontal: spacingX._20,
+        marginTop: verticalScale(8)
+    },
+    header:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: spacingY._10
+    },
+    searchIcon:{
+        backgroundColor: colors.neutral700,
+        padding: spacingX._10,
+        borderRadius: 50
+    },
+    floatingButton:{
+        height: verticalScale(50),
+        width: verticalScale(50),
+        borderRadius: 100,
+        position: 'absolute',
+        bottom: verticalScale(30),
+        right: verticalScale(30),
+    },
+    scrollVeiwStyle:{
+        marginTop: spacingY._10,
+        paddingBottom: verticalScale(100),
+        gap: spacingY._25
+    }
+})
