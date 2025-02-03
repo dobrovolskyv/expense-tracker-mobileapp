483ba2cfffee443d7378b215bb8bcfa0d6e1a7fe
Vlados <malak3@mail.ru>
2025-02-03 20:19:01 +0300

    add homescreen layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, ScrollView, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import Button from '@/components/Button'
 import Typograph from '@/components/Typograph'
@@ -9,8 +9,12 @@ import ScreenWrapper from '@/components/ScreenWrapper'
 import { useAuth } from '@/contexts/authContext'
 import { verticalScale } from '@/utils/styling'
 
+import AntDesign from '@expo/vector-icons/AntDesign';
+import HomeCard from '@/components/HomeCard'
+
+
 const Home = () => {
-    const {user} = useAuth() 
+    const { user } = useAuth()
 
     // const handleLogout = async () => {
     //     await signOut(auth)
@@ -18,8 +22,27 @@ const Home = () => {
 
     return (
         <ScreenWrapper>
-            <Typograph>Home</Typograph>
-        
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <View style={{ gap: 4 }}>
+                        <Typograph size={16} color={colors.neutral400}>Hello,</Typograph>
+                        <Typograph size={20} fontWeight={"500"}>{user?.name}</Typograph>
+                    </View>
+                    <TouchableOpacity style={styles.searchIcon}>
+                        <AntDesign name="search1" size={verticalScale(22)} color={colors.neutral200} />
+                    </TouchableOpacity>
+                </View>
+
+                <ScrollView
+                    contentContainerStyle={styles.scrollVeiwStyle}
+                    showsVerticalScrollIndicator={false}
+                >
+                    <View>
+                        <HomeCard />
+                    </View>
+                </ScrollView>
+            </View>
+
         </ScreenWrapper>
     )
 }
@@ -27,23 +50,23 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         paddingHorizontal: spacingX._20,
         marginTop: verticalScale(8)
     },
-    header:{
+    header: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         marginBottom: spacingY._10
     },
-    searchIcon:{
+    searchIcon: {
         backgroundColor: colors.neutral700,
         padding: spacingX._10,
         borderRadius: 50
     },
-    floatingButton:{
+    floatingButton: {
         height: verticalScale(50),
         width: verticalScale(50),
         borderRadius: 100,
@@ -51,7 +74,7 @@ const styles = StyleSheet.create({
         bottom: verticalScale(30),
         right: verticalScale(30),
     },
-    scrollVeiwStyle:{
+    scrollVeiwStyle: {
         marginTop: spacingY._10,
         paddingBottom: verticalScale(100),
         gap: spacingY._25
